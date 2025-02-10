5a1d6b695a4d9b111aab54a6b9feed71272dba3a
Kim St-Pierre <kim.st-pierre.2@ulaval.ca>
2025-02-09 23:01:02 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true,
+    swcMinify: true,
+    output: "standalone",
+    webpack(config) {
+        return config;
+    },
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+    },
 };
 
 export default nextConfig;
