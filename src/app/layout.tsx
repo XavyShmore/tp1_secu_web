51bb97ed52cc0037c01fd0d5f1724dbae997f00a
Kim St-Pierre <kim.st-pierre.2@ulaval.ca>
2025-02-13 20:52:17 -0500

    quick fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type {Metadata} from "next";
-import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import Footer from "@/app/components/footer";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
     title: "TP1 secu web",
@@ -24,7 +13,7 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+            className={`antialiased flex flex-col min-h-screen`}
         >
         <main className="flex-1">{children}</main>
         <Footer/>
