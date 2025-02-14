d536b71e651257d550c73819dfc98d2d48be76cf
Kim St-Pierre <kim.st-pierre.2@ulaval.ca>
2025-02-13 21:37:22 -0500

    valid data

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -11,10 +11,20 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({ message: 'All fields are required.' }, { status: 400 });
     }
 
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$/;
+    if (!emailRegex.test(email)) {
+        return NextResponse.json({ message: 'Invalid email format. a@a.a requested' }, { status: 400 });
+    }
+
+
     const existingUser = await prisma.user.findUnique({ where: { email } });
 
     if (existingUser) {
         return NextResponse.json({ message: 'This email is already used.' }, { status: 400 });
+    }
+
+    if (password.length < 6 || !/[A-Z]/.test(password) || !/\d/.test(password)) {
+        return NextResponse.json({message: 'Password is too simple.' }, {status: 400})
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
