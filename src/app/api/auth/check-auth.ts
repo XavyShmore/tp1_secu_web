814bf89662e13abbcb887489ed77083065095254
Xavier Hamel <xavierhamel02+git@gmail.com>
2025-04-06 02:24:41 -0400

    feature: ajouter les session par token au lieu d'utiliser les userId comme token de session.

---
+++ src/app/api/auth/check-auth.ts
@@ -0,0 +1,39 @@
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+export default async function checkAuth( sessionToken: string | undefined): Promise<string> {
+
+    //const protectedRoutes = ["/hidden", "/api/profile", "/api/task"];
+
+    if (!sessionToken) {
+        return Promise.reject(new Error("Session token is missing"));
+    }
+
+    console.log("Cookie user:", sessionToken);
+
+    const session = await prisma.session.findUnique({
+        where: {
+            token: sessionToken
+        }
+    });
+
+    if (session === null){
+        return Promise.reject(new Error("Invalid User"));
+    }
+
+    const _MS_PER_HOUR = 1000 * 60 * 60;
+    const tokenAge = Date.UTC(Date.now()) - Date.UTC(session.createdAt.getUTCDate())
+
+    if (tokenAge > _MS_PER_HOUR){
+        prisma.session.deleteMany({
+            where: {
+                token: sessionToken
+            }
+        });
+        return Promise.reject(new Error("Session expired"));
+    }
+
+    return Promise.resolve(session.userId);
+}
